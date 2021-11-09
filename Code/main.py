93e6f4c425ea93b464816b5627ce5ce87a26a5ff
Jack Malone <c00236428@itcarlow.ie>
2021-11-09 21:04:36 +0000

    working on the starter code for the project

--- Code/main.py
+++ Code/main.py
@@ -16,9 +16,18 @@ def main():
     board = Board(background=background, size=19)
 
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        pygame.event.pump()
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
                 running = False
+        elif event.type == pygame.VIDEORESIZE:
+            width, height = event.size
+            if width < 800:
+                width = 800
+            if height < 800:
+                height = 800
+            screen = pygame.display.set_mode((width,height), RESIZABLE)
+            
         screen.fill((255,255,255))
         board.render(screen)
         pygame.display.flip()
