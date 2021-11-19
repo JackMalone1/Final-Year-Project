0b265e8a3112b35f88fb19e76e30043d79013894
Jack Malone <c00236428@itcarlow.ie>
2021-11-19 12:37:37 +0000

    added in gui library

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,7 @@ from os import system
 import pygame
 from pygame.constants import FULLSCREEN, RESIZABLE
 from board import Board
+import pygame_gui
 
 def main():
     pygame.init()
@@ -14,25 +15,36 @@ def main():
     running = True
     background = pygame.image.load("Assets//background.jpg")
     board = Board(background=background, size=19)
-
+    manager = pygame_gui.UIManager((800, 800))
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                             text='Say Hello',
+                                             manager=manager)
+    clock = pygame.time.Clock()
     while running:
-        pygame.event.pump()
-        event = pygame.event.wait()
-        if event.type == pygame.QUIT:
-                running = False
-        elif event.type == pygame.VIDEORESIZE:
-            width, height = event.size
-            if width < 800:
-                width = 800
-            if height < 800:
-                height = 800
-            screen = pygame.display.set_mode((width,height), RESIZABLE)
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            board.checkMousePosition(pygame.mouse.get_pos())
-
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    running = False
+            elif event.type == pygame.VIDEORESIZE:
+                width, height = event.size
+                if width < 800:
+                    width = 800
+                if height < 800:
+                    height = 800
+                screen = pygame.display.set_mode((width,height), RESIZABLE)
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                board.checkMousePosition(pygame.mouse.get_pos())
+            
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == hello_button:
+                        print("hello world")
+            manager.process_events(event)
+        manager.update(time_delta)
             
         screen.fill((255,255,255))
         board.render(screen)
+        manager.draw_ui(screen)
         pygame.display.flip()
     
     pygame.quit()
