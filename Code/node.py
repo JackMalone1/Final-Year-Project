d8352ed23964d29984d480a7686ba9e4b8776ce4
Jack Malone <c00236428@itcarlow.ie>
2021-12-31 15:24:40 +0000

    Adding in extra init functions to the node class

--- Code/node.py
+++ Code/node.py
@@ -1,18 +1,35 @@
 from board import Board
+from colours import Colour
 from playerturn import PlayerTurn
 from copy import deepcopy, copy
 import numpy as np
 
 
 class Node:
-    def __init__(self, board: Board, player: PlayerTurn):
+    def __init__(self):
         self.parent = None
         self.score = 0
+        self.visited = 0
         self.children = []
-        self.board = copy.deepcopy(board)
-        self.player = player
         self.total_simulations = 0
         self.won_simulations = 0
+
+    def __init__(self, board: Board, player: PlayerTurn):
+        self.__init__(self)
+        self.parent = None
+        self.possible_moves = []
+        self.board = copy.deepcopy(board)
+        self.player = player
+
+    def __init__(self, parent, player: PlayerTurn, position: tuple):
+        self.__init__(self)
+        self.parent = parent
+        self.player = player
+        self.position = position
+        self.board = copy.deepcopy(parent.board)
+        self.current_colour = Colour.WHITE if player is PlayerTurn.WHITE else Colour.BLACK
+        if self.board.piece_matrix[position[0]][position[1]].colour is Colour.CLEAR:
+            self.board.piece_matrix[position[0]][position[1]].colour = self.current_colour
 
     def backup(self, evaluation):
         pass
